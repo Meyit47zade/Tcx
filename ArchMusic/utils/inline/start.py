cfa57adc95234a0987aab3af3ca5ee989a2ab308
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-30 16:18:54 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -113,7 +113,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"https://github.com/ArchBots/ArchMusic"
+                        text=_["S_B_6"], url=f"https://github.com/Meyit47zade/tcx"
                     ),
                 ]
             )
