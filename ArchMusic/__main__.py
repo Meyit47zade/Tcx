66aab1891931db0f9468624a9743e2a1369e26bc
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-05-05 10:23:08 +0300

    Update __main__.py

--- ArchMusic/__main__.py
+++ ArchMusic/__main__.py
@@ -24,21 +24,6 @@ from ArchMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 
-from pytgcalls import PyTgCalls
-from pytgcalls import idle
-from pytgcalls.types import MediaStream
-...
-chat_id = -1001185324811
-app = PyTgCalls(client)
-app.start()
-app.play(
-    chat_id,
-    MediaStream(
-        'http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4',
-    )
-)
-idle()
-
 async def init():
     if (
         not config.STRING1
